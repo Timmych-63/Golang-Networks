53a86652150eeb79a8dda387cb449fed251b9529
Timmych-63 <shabardin.timur.iva@gmail.com>
2026-06-15 20:45:13 +0400

    Add a code in new file

---
+++ Task1/server-render/This is a new one for checking pull requests.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		pageData := struct {
+			Title   string
+			Heading string
+			Text    string
+		}{
+			Title:   "Это главная страница, пока что она одна =(, сейчас)",
+			Heading: "Hellov. Hov are u?",
+			Text:    "Это текст из файла main.go, можешь проверить в структуре. Он был создан, когда ты сделал запрос к сайту. Вот так то!",
+		}
+
+		tmpl, err := template.ParseFiles("index.html")
+		if err != nil {
+			http.Error(w, "Ошибочка вышла: ", http.StatusInternalServerError)
+			return
+		}
+
+		tmpl.Execute(w, pageData)
+	})
+
+	fmt.Println("Server is running on 8080")
+
+	http.ListenAndServe(":8080", nil)
+}
